1fbe69783c144b8a049146552d2617e56e80f9b0
huazheleyoushang <33747307+huazheleyoushang@users.noreply.github.com>
2024-09-26 13:42:20 +0000

    Deploying to gh-pages from @ huazheleyoushang/react-admin@bf6fcc3428f20ed5a064792fd37b2f74ce41a151 🚀

--- assets/js/index-Bm3Qyorm.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CJmw8spj.js";const t=()=>e.jsx("div",{className:"card content-box",children:e.jsx("span",{className:"text",children:"Menu222 🍓🍇🍈🍉"})});export{t as default};
